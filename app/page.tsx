3e68cc20ab22d2d297986547d3eac013efabf9fc
Filip <filipstevanovic@gmail.com>
2025-05-15 00:07:16 +0200

    ads

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import ProcessSection from "@/components/Process";
 import Services from "@/components/Services";
@@ -8,12 +6,10 @@ import Services from "@/components/Services";
 export default function Home() {
   return (
     <div className="min-h-screen font-sans">
-      <Header />
       <Hero />
       <Services />
       <ProcessSection />
       <Contact />
-      <Footer />
     </div>
   );
 }
