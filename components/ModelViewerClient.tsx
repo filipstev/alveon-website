538a40a7c73e36e8e9f5c071cd98ba825c675d05
Filip <filipstevanovic@gmail.com>
2025-05-17 01:48:46 +0200

    a

--- components/ModelViewerClient.tsx
+++ components/ModelViewerClient.tsx
@@ -1,10 +1,19 @@
 "use client";
 
 import "@google/model-viewer";
+import { useState } from "react";
 
 export default function ModelViewerClient() {
+  const [showBgImage, setShowBgImage] = useState(true);
   return (
     <div className="relative w-full h-full">
+      <div
+        className="absolute inset-0 w-full h-full bg-cover bg-center"
+        style={{
+          backgroundImage: `url(/models/poster.webp)`,
+          display: showBgImage ? "block" : "none",
+        }}
+      />
       {/* @ts-ignore */}
       <model-viewer
         src="/models/alvi.glb"
@@ -15,9 +24,10 @@ export default function ModelViewerClient() {
         environment-image="neutral"
         camera-orbit="90deg 75deg 2.5m"
         poster="/models/poster.webp"
-        // onload={() => {
-        //   (window as any).modelLoaded = true;
-        // }}
+        onload={() => {
+          (window as any).modelLoaded = true;
+          setShowBgImage(false);
+        }}
         style={{
           width: "100%",
           height: "100%",
